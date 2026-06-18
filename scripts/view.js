536c6f48485e7d3e2e4ae7c865b7523863021e6a
Instel <135399235+Instel12@users.noreply.github.com>
2026-06-18 09:50:13 -0400

    added more net framework versions

--- scripts/view.js
+++ scripts/view.js
@@ -11,7 +11,7 @@ const titles = [
     "Amanda Extensions"
 ]
 const description = [
-    "Extendable Menu is a menu that other mods add on to externally.<p>I will maybe make documentations on how to add your own mods later, however I don't use obfuscation so you may be able to decompile it and find out.<p>The default hotkey is [`] but it is configurable inside of the menu's config file.<p>This mod's only dependancy is BepInEx.<p><a href='files/ExtendableMenu/1.1.0/Extendable.Menu.dll'>Download 1.1.0</a><p><a href='files/ExtendableMenu/1.0.0/Extendable.Menu.dll'>Download 1.0.0</a>",
+    "Extendable Menu is a menu that other mods add on to externally.<p>I will maybe make documentations on how to add your own mods later, however I don't use obfuscation so you may be able to decompile it and find out.<p>The default hotkey is [`] but it is configurable inside of the menu's config file.<p>This mod's only dependancy is BepInEx.<p><a href='files/ExtendableMenu/1.1.1/Extendable.Menu.Net35.dll'>Download 1.1.1 Net35</a> | <a href='files/ExtendableMenu/1.1.1/Extendable.Menu.Net48.dll'>Download 1.1.1 Net48</a><p><a href='files/ExtendableMenu/1.1.0/Extendable.Menu.dll'>Download 1.1.0</a><p><a href='files/ExtendableMenu/1.0.0/Extendable.Menu.dll'>Download 1.0.0</a>",
     "Amanda Extensions are some mods for Amanda the Adventurer (the first one) for Extendable Menu.<p>Currently, the mods are: Object Minigun, Grab Random Object, Load Random Tape, Force Eject Tape, Unlock All Stickers, Piano Spam, Enable All Tapes, Summon All Tapes and Spawn Wooly Secret Tape<p>This mod depends on BepInEx and Extendable Menu<p><a href='files/AmandaExtensions/1.0.0/Amanda.Extensions.dll'>Download 1.0.0</a>"
 ]
 
